1379ed32194240666caf149d84cfb4aeebe8a8ce
warlockee <warlock.lxy@gmail.com>
2023-11-06 14:40:17 -0800

    fix setup

--- install-bashland.sh
+++ install-bashland.sh
@@ -64,7 +64,7 @@ fish_config="${XDG_CONFIG_HOME:-~/.config}/fish/config.fish"
 
 # Optional parameter to specify a github branch
 # to pull from.
-github_branch=${1:-'0.0.10'}
+github_branch=${1:-'0.0.11'}
 
 install_bashland() {
     check_dependencies
