199da753c027a722a2feaa594270af8c04f1245b
warlockee <warlock.lxy@gmail.com>
2023-11-06 14:18:29 -0800

    add CI

--- install_bashland.py
+++ install_bashland.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# Python version of install bashhub
+# Python version of install bashland
 #
 
 import os
@@ -10,30 +10,30 @@ from pkg_resources import resource_exists, resource_filename
 
 
 def check_already_installed(home_dir):
-    installed = os.path.isdir(home_dir + "/.bashhub")
+    installed = os.path.isdir(home_dir + "/.bashland")
     if installed:
-        raise RuntimeError("Looks like bashhub is already installed. " + \
-                "rm -r ~/.bashhub to install again.")
+        raise RuntimeError("Looks like bashland is already installed. " + \
+                "rm -r ~/.bashland to install again.")
 
 
-def setup_bashhub_files(home_dir):
-    bashhub_dir = home_dir + '.bashhub/'
+def setup_bashland_files(home_dir):
+    bashland_dir = home_dir + '.bashland/'
     # move .sh files to appropriate place.
-    exists = resource_exists(__name__, 'bashhub/shell')
+    exists = resource_exists(__name__, 'bashland/shell')
     if exists:
-        shell_scripts = resource_filename(__name__, 'bashhub/shell')
-        # Should create our bashhub directory and copy our files there.
-        shutil.copytree(shell_scripts, bashhub_dir)
+        shell_scripts = resource_filename(__name__, 'bashland/shell')
+        # Should create our bashland directory and copy our files there.
+        shutil.copytree(shell_scripts, bashland_dir)
 
         # Add our file to our bash config if it's not present
         bash_config = find_users_bash_config(home_dir)
         with open(bash_config, "a+") as config:
-            source_hook = "source ~/.bashhub/bashhub.sh"
+            source_hook = "source ~/.bashland/bashland.sh"
             if source_hook not in config.read():
                 config.write(source_hook)
 
     else:
-        raise RuntimeError("Couldn't install bashhub files in " + bashhub_dir)
+        raise RuntimeError("Couldn't install bashland files in " + bashland_dir)
 
 
 def find_users_bash_config(home_dir):
@@ -53,7 +53,7 @@ def main():
         home = os.environ["HOME"] + '/'
         find_users_bash_config(home)
         check_already_installed(home)
-        setup_bashhub_files(home)
+        setup_bashland_files(home)
     except Exception as err:
         sys.stderr.write('Setup Error:\n%s\n' % str(err))
         sys.exit(1)
