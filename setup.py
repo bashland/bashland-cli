c4016bd13ee80f1a619a2224fa8427881d9cbdbf
warlockee <warlock.lxy@gmail.com>
2023-11-06 14:38:31 -0800

    fix setup

--- setup.py
+++ setup.py
@@ -12,7 +12,12 @@ setup(
     author_email='bash.land@hotmail.com',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[],
+    install_requires=[
+        'requests==2.23.0', 'jsonpickle==3.0.1', 'click==6.7',
+        'npyscreen==4.10.5', 'python-dateutil==2.8.1',
+        'pymongo==3.10.1', 'inflection==0.3.1', 'humanize==1.0.0',
+        'future==0.18.3', 'mock==3.0.5'
+    ],
     entry_points={
         'console_scripts': ['bl=bashland.bl:main',
                             'bashland=bashland.bashland:main']
